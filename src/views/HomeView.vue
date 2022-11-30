97177a28cbc1fec694c221331ec6f16c8b2a35c7
Arsipa <senya.suvorov2@gmail.com>
2022-11-30 16:34:36 +0300

    add video, change stack to vanila vue3

---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="home">
+        <video loop autoplay muted poster="@/assets/video/mainVideoPreview.png">
+          <source src="@/assets/video/back.mp4" type="video/mp4" />
+          <source src="@/assets/video/back.webm" type="video/webm" />
+        </video>
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+    name: "HomeView",
+    components: {
+        HelloWorld,
+    },
+};
+</script>
+
+<style scoped>
+video {
+    width: 100%;
+}
+</style>
